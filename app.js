4fa779409a7ee118ce32a9f2c9eca9a6fab4e813
RainBoard01 <amdmsi4ever@hotmail.com>
2019-08-19 17:58:46 -0400

    Installed cors for koa

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('koa-bodyparser');
 const { errorHandler, execute } = require('graphql-api-koa');
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://rainboard:.Amdapua45300@graphql-rn775.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const cors = require('@koa/cors');
 const schema = require('./schema');
 
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -12,5 +13,7 @@ const app = new Koa()
     .use(errorHandler())
     .use(bodyParser())
     .use(execute({ schema }));
+
+app.use(cors());
 
 app.listen(4000, () => console.log('Listening on Port 4000'));
