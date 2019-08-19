c9ac8ea4d4afd90130c0ecce6d214c806f6388b7
RainBoard01 <amdmsi4ever@hotmail.com>
2019-08-19 17:50:01 -0400

    Koa version of BooksApp server

---
+++ app.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const { errorHandler, execute } = require('graphql-api-koa');
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://rainboard:.Amdapua45300@graphql-rn775.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const schema = require('./schema');
+
+mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log("DB connected"));
+
+const app = new Koa()
+    .use(errorHandler())
+    .use(bodyParser())
+    .use(execute({ schema }));
+
+app.listen(4000, () => console.log('Listening on Port 4000'));
